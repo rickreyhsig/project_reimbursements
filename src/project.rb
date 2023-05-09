3a28ba8a85babf46ec421d87429d3411923b59d3
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2023-05-09 02:16:56 +0000

    Rm project unnecessary helpers

--- src/project.rb
+++ src/project.rb
@@ -1,5 +1,4 @@
 require 'date'
-require 'byebug'
 
 class Project
   attr_accessor :start_date, :end_date, :city_type
@@ -12,17 +11,5 @@ class Project
 
   def total_days
     (end_date - start_date + 1).to_i
-  end
-
-  def travel_days
-    total_days > 2 ? 2 : 1
-  end
-
-  def full_days(previous_project_end_date = nil)
-    if previous_project_end_date.nil?
-      (end_date - start_date - 1).to_i
-    else
-      (end_date - previous_project_end_date - 1).to_i
-    end
   end
 end
--- tests/project_test.rb
+++ tests/project_test.rb
@@ -1,20 +1,11 @@
 require_relative '../src/project'
 require 'test/unit'
 require 'date'
-# require 'byebug'
 
 class TestProjects < Test::Unit::TestCase
-  @@project = Project.new(Date.new(2023, 5, 1), Date.new(2023, 5, 10), :low)
-
   def test_total_days
-    assert_equal( @@project.total_days, 10 )
-  end
+    project = Project.new(Date.new(2023, 5, 1), Date.new(2023, 5, 10), :low)
 
-  def test_travel_days
-    assert_equal( @@project.travel_days, 2 )
-  end
-
-  def test_full_days
-    assert_equal( @@project.full_days, 8 )
+    assert_equal( project.total_days, 10 )
   end
 end
